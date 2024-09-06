ffe92b21fd99a13e8c67f152b15eef29c5d76642
Kirandinesh <kirandinesh646@gmail.com>
2024-09-07 01:20:14 +0530

    calculator

--- calc.js
+++ calc.js
@@ -1,27 +1,42 @@
-function pressed(value) {
-  var display = document.getElementById('display');
-  display.value += value;
-}
-
-function calculate() {
-  var display = document.getElementById('display');
-  if (display.value == '') {
-    display.value = 0;
-  } else {
-    display.value = eval(display.value);
-  }
-}
-
 function clearDisplay() {
-  var display = document.getElementById('display');
-  display.value = '';
+  document.getElementById('result').value = '';
 }
 
 function back() {
-  var display = document.getElementById('display');
-  display.value = display.value.slice(0, -1);
+  let currentValue = document.getElementById('result').value;
+  document.getElementById('result').value = currentValue.slice(0, -1);
 }
 
-document.querySelector('.toggle').addEventListener('click', function () {
-  document.body.classList.toggle('light-mode');
-});
+function pressed(value) {
+  document.getElementById('result').value += value;
+}
+
+function calculate() {
+  let expression = document.getElementById('result').value;
+  try {
+    let result = eval(expression);
+    if (result === Infinity || result === -Infinity || isNaN(result)) {
+      throw new Error();
+    }
+    document.getElementById('result').value = result;
+  } catch (e) {
+    document.getElementById('result').value = ''; 
+  }
+}
+
+
+function toggleMode() {
+  const body = document.body;
+  const modeButton = document.querySelector('.mode-button');
+  var mode = document.getElementById('mode-button')
+
+  body.classList.toggle('dark-mode');
+  
+  if (body.classList.contains('dark-mode')) {
+    modeButton.innerHTML = 'Light<br>Mode';
+    document.getElementById('result').style.color='white'
+  } else {
+    modeButton.innerHTML = 'Dark<br>Mode';
+    document.getElementById('result').style.color='black'
+  }
+}
